a0356993eb1ed509a3dd24dac40d556b0230f840
Kyle DM <confidential.inc@gmail.com>
2017-06-27 20:28:48 -0600

    adjusted structure + bower + npm

--- public/ng/app.js
+++ public/ng/app.js
@@ -3,6 +3,10 @@
 **      PMFLOW -- Project Manager Flow        **
 ** ------------------------------------------ **
 **  Copyright (c) 2017 - Kyle Derby MacInnis  **
+**                                            **
+** Any unauthorized distribution or transfer  **
+**    of this work is strictly prohibited.    **
+**                                            **
 **           All Rights Reserved.             **
 ** ------------------------------------------ **
 \*                                            */
